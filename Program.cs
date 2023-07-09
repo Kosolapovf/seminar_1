a283c37c5316a331ae854910245f7489d082833c
Egor <EgorrKosolapoff@ya.ru>
2023-07-09 15:24:48 +0700

    Вывод из трехзначного числа последнюю цифру

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
 ﻿Console.Clear();
 Console.Write("Введите число:");
 int N = Convert.ToInt32(Console.ReadLine());
-int i = -1 * N;
-while (i <= N)
-{
-    //форматирование строк
-    Console.Write($"{i} ");
-    i ++;
-}
+//деление и вывод остатка, i будет выводить остаток
+int i = N % 10;
+Console.Write(i);
