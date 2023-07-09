25ffa8ab2d501cdee33b632329bcfabed93e19cf
Egor <EgorrKosolapoff@ya.ru>
2023-07-09 15:18:26 +0700

    перечисление чисел от -N до N

--- Program.cs
+++ Program.cs
@@ -1,19 +1,10 @@
-﻿
-Console.Write("Введите число от 1 до 7:");
-int a = Convert.ToInt32(Console.ReadLine());
-if (a == 1)
-    Console.WriteLine("Понедельник");
-else if (a == 2)
-    Console.WriteLine("Вторник");
-else if (a == 3)
-    Console.WriteLine("Среда");
-else if (a == 4)
-    Console.WriteLine("Четверг");
-else if (a == 5)
-    Console.WriteLine("Пятница"); 
-else if (a == 6)
-    Console.WriteLine("Суббота");
-else if (a == 7)
-    Console.WriteLine("Воскерсенье");  
-else
-    Console.WriteLine("Соблюдай условие!!!");
+﻿Console.Clear();
+Console.Write("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = -1 * N;
+while (i <= N)
+{
+    //форматирование строк
+    Console.Write($"{i} ");
+    i ++;
+}
